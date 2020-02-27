0298967dd8fb733fc70611d193244f2840d37a75
Tracy Collins <tc@threeceemedia.com>
2020-02-27 03:41:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(text){
 function whisper(text){
   return text.toLowerCase();
 }
+
+function logShout(text){
+  console.log(text.toUpperCase());
+}
+
+function logWhisper(text){
+  console.log(text.toLowerCase());
+}
